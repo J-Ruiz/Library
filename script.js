3a62bdd5c98e6f32378b233321dd77ca87feb135
Jake Ruiz <jwr318@gmail.com>
2022-06-05 21:27:20 -0400

    created containers, header and add book button with appropriate stylings to create the skeleton of the add book app.

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let bookContainer = document.getElementById("book-container");
+let addBookButton = document.getElementById("add-book");
+
+addBookButton.addEventListener("click", event=>{
+    // add a pop up form https://www.w3schools.com/howto/howto_js_popup_form.asp
+})
